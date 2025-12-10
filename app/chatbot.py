676c0dd214a63ea61eb22483126afe7f641185ea
ABHISHEK B SHETTY <abhishetty1506@gmail.com>
2025-12-10 23:34:13 +0530

    ollama chatbot

--- app/chatbot.py
+++ app/chatbot.py
@@ -1,25 +1,21 @@
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
-from dotenv import load_dotenv
+from langchain_ollama import ChatOllama
 
-load_dotenv()
-
-
-
-models = ChatGoogleGenerativeAI(model = "gemma-3-12b-it")
+model = ChatOllama(model="gemma3:1b")   # or "gemma:instruct", "mistral:instruct"
 
 chat_history = [
-    SystemMessage(content = "your name is abhishek b shetty and you are helpfull ai agent")
-
-
+    {"role": "system", "content": "your name is abhishek b shetty and you are a helpful ai agent"}
 ]
 
-
 while True:
-    a = input("You : ")
-    chat_history.append(HumanMessage(content = a))
-    if(a == "exit"):
+    user_input = input("You : ")
+
+    if user_input == "exit":
         break
-    result = models.invoke(str(chat_history))
-    chat_history.append(AIMessage(result.content))
-    print("Bot : ",result.content)
+
+    chat_history.append({"role": "user", "content": user_input})
+
+    response = model.invoke(chat_history)
+
+    print("Bot :", response.content)
+
+    chat_history.append({"role": "assistant", "content": response.content})
