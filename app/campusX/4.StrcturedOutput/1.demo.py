d351eff6b467361d1c580dbb8c54070bc8a446dd
ABHISHEK B SHETTY <abhishetty1506@gmail.com>
2025-12-30 22:30:29 +0530

    used pydantic

--- app/campusX/4.StrcturedOutput/1.demo.py
+++ app/campusX/4.StrcturedOutput/1.demo.py
@@ -1,17 +1,29 @@
 from typing import TypedDict
+from pydantic import BaseModel , EmailStr, Field
+from typing import Optional
 
 
+class Student(BaseModel):
+    name : str = "abhishek"
+    rollno : int
+    age : Optional[str] = None
+    mail : EmailStr
+    cgpa : float = Field(gt = 0,lt = 10,default = 5,description = "A decimal value ") 
+   
 
 class Person(TypedDict):
     name : str 
     age : int 
 
+new_student = {"rollno":78,"mail":"asmnxbc@gm.com"}
+
+student = Student(**new_student)
 
 new_person : Person = {"name":"abhishek","age":18}
 
 
+print(student)
+
 print(new_person)
-
-
 
 
