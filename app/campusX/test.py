ee4b38ad9b3f10735741d4a3e10ff0607bb186f8
ABHISHEK B SHETTY <abhishetty1506@gmail.com>
2026-01-02 14:58:19 +0530

    learning of vector db

--- app/campusX/test.py
+++ app/campusX/test.py
@@ -1,3 +1,14 @@
-import langchain
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_ollama import ChatOllama
 
-print(langchain.__version__)
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    max_output_tokens=10   # HARD CAP
+)
+
+
+response = llm.invoke("Write a  poem about cricket.")
+print(response.content)
